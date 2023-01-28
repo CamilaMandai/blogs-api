3cc4de41d4d8530263518004720281ba436dee40
Camila Mandai <cayumandai@gmail.com>
2023-01-28 15:48:53 -0300

    feat: implementa camada models de PostCategory

---
+++ src/models/PostCategory.js
@@ -0,0 +1,35 @@
+const PostCategory = (sequelize, DataTypes) => {
+  const PostCategoryTable = sequelize.define(
+    'PostCategory',
+    {
+      postId: DataTypes.INTEGER,
+      categoryId: DataTypes.INTEGER,
+  },
+    {
+      timestamps: false,
+      tableName: 'posts_categories',
+      underscored: true
+    });
+
+  PostCategoryTable.associate = ({ BlogPost, Category }) => {
+    BlogPost.belongsToMany(Category, {
+      as: 'categories',
+      // a FK sempre sera o model da esquerda
+      foreignKey: 'post_id',
+      otherKey: 'category_id',
+      through: PostCategoryTable
+    });
+
+    Category.belongsToMany(BlogPost, {
+      as: 'blogPosts',
+      // a FK sempre sera o model da esquerda
+      foreignKey: 'category_id',
+      otherKey: 'post_id',
+      through: PostCategoryTable
+    });
+  }
+
+  return PostCategoryTable;
+}
+
+module.exports = PostCategory;
